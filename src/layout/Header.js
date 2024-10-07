582cf725fd8cbfd38bd1b0f9ce391cda14e5c2f4
Niklathiya <lathiya810@gmail.com>
2024-10-07 23:21:46 +0530

    .

--- src/layout/Header.js
+++ src/layout/Header.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import "./layout.css";
 import { Link } from "react-router-dom";
 import { Logo, Search, Cart, Profile, Menuicon } from "../image";
